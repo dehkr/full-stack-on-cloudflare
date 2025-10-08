ab1277d19c18d2c45e3a390fb16441f97f5064b2
dehkr <dehkr25@gmail.com>
2025-10-07 22:40:02 -0500

    Handle retrieving and storing link info with KV

--- apps/data-service/src/helpers/route-ops.ts
+++ apps/data-service/src/helpers/route-ops.ts
@@ -1,15 +1,65 @@
-import { LinkSchemaType } from '@repo/data-ops/zod-schema/links';
+import { getLink } from '@repo/data-ops/queries/links';
+import { linkSchema, LinkSchemaType } from '@repo/data-ops/zod-schema/links';
 
+const TTL_TIME = 60 * 60 * 24; // 1 day
+
+/**
+ * Get links from KV store.
+ */
+async function getLinkInfoFromKv(env: Env, id: string) {
+  const parsedLinkInfo = await env.CACHE.get(id, { type: 'json' });
+  if (!parsedLinkInfo) {
+    return null;
+  }
+  try {
+    return linkSchema.parse(parsedLinkInfo);
+  } catch (error) {
+    console.error('Link schema validation failed:', error);
+    return null;
+  }
+}
+
+/**
+ * Save link info to KV store.
+ */
+async function saveLinkInfoToKv(env: Env, id: string, linkInfo: LinkSchemaType) {
+  try {
+    await env.CACHE.put(id, JSON.stringify(linkInfo), {
+      expirationTtl: TTL_TIME,
+    });
+  } catch (error) {
+    console.error('Error saving link info to KV:', error);
+    throw error;
+  }
+}
+
+/**
+ * Check KV first. If not in KV, retrieve from DB then store in KV.
+ */
+export async function getRoutingDestinations(env: Env, id: string) {
+  const linkInfo = await getLinkInfoFromKv(env, id);
+  if (linkInfo) {
+    return linkInfo;
+  }
+  const linkInfoFromDb = await getLink(id);
+  if (!linkInfoFromDb) {
+    return null;
+  }
+  await saveLinkInfoToKv(env, id, linkInfoFromDb);
+
+  return linkInfoFromDb;
+}
+
+/**
+ * Check if country code exists in destinations. If none exists, use default.
+ */
 export function getDestinationForCountry(linkInfo: LinkSchemaType, countryCode?: string) {
   if (!countryCode) {
     return linkInfo.destinations.default;
   }
-
-  // Check if the country code exists in destinations
   if (linkInfo.destinations[countryCode]) {
     return linkInfo.destinations[countryCode];
   }
 
-  // Fallback to default
   return linkInfo.destinations.default;
 }
