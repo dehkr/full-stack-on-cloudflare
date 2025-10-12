77c33f93f78e20952e21aca6e74cd637db3689ec
dehkr <dehkr25@gmail.com>
2025-10-12 13:59:57 -0500

    Save AI output to DB

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -1,6 +1,8 @@
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
 import { collectDestinationInfo } from '@/helpers/browser-render';
 import { aiDestinationChecker } from '@/helpers/ai-destination-checker';
+import { addEvaluation } from '@repo/data-ops/queries/evaluations';
+import { initDatabase } from '@repo/data-ops/database';
 
 export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<
   Env,
@@ -10,7 +12,8 @@ export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<
     event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>,
     step: WorkflowStep,
   ) {
-    
+    initDatabase(this.env.DB);
+
     const collectedData = await step.do(
       'Collect rendered destination page data',
       async () => {
@@ -30,6 +33,27 @@ export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<
         return await aiDestinationChecker(this.env, collectedData.bodyText);
       },
     );
+
+    const evaluationId = await step.do(
+      'Save evaluation in database',
+      async () => {
+        return await addEvaluation({
+          linkId: event.payload.linkId,
+          status: aiStatus.status,
+          reason: aiStatus.statusReason,
+          accountId: event.payload.accountId,
+          destinationUrl: event.payload.destinationUrl,
+        });
+      },
+    );
+
+    // await step.do('Backup destination HTML in R2', async () => {
+    //   const accountId = event.payload.accountId;
+    //   const r2PathHtml = `evaluations/${accountId}/html/${evaluationId}`;
+    //   const r2PathBodyText = `evaluations/${accountId}/body-text/${evaluationId}`;
+    //   await this.env.BUCKET.put(r2PathHtml, collectedData.html);
+    //   await this.env.BUCKET.put(r2PathBodyText, collectedData.bodyText);
+    // });
 
     console.log(collectedData);
   }
